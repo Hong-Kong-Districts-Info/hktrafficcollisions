f51277c65ebae0d8dbe25d1f025a8ae273cbc2c5
Gabriel Tam <gabrieltamml@gmail.com>
2020-08-27 13:29:50 +0100

    Added content to 2 menu items
    Menu items include:
    - Collision Location Map
    - Dashboard

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -99,10 +99,236 @@ ui <- dashboardPage(
   body = dashboardBody(
     tabItems(
 
-      # Tab: Overview ----------------------------------------------------------
+      # Tab: Collision Location Map --------------------------------------------
 
       tabItem(
-        tabName = "tab_overview"
+        tabName = "tab_collision_location_map",
+        fluidRow(
+          box(
+            width = 4,
+            title = "Location Filter",
+            "Insert location filter here"
+          )
+        ),
+        fluidRow(
+          box(
+            width = 8,
+            title = "Map",
+            "Insert map here"
+          ),
+          box(
+            width = 4,
+            title = "Stats & Info",
+            "Insert data here"
+          )
+        )
+      ),
+
+      # Tab: Dashboard ---------------------------------------------------------
+
+      tabItem(
+        tabName = "tab_dashboard",
+        tabBox(
+          width = 12,
+
+          # All Vehicle Collision tab
+          tabPanel(
+            value = "all_vehicle_collision",
+            title = "All Vehicle Collision",
+
+            fluidRow(
+              box(
+                width = 4,
+                title = "Area Filter",
+                "Insert area filter here"
+              ),
+              box(
+                width = 4,
+                title = "Year Filter",
+                "Insert year filter here"
+              ),
+              box(
+                width = 4,
+                title = "All/ KSI Filter",
+                "Insert all/ksi filter here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                  width = 6,
+                  title = "Collision Map",
+                  "Insert collison map here"
+              ),
+              box(width = 6,
+                  title = "KSI Stats",
+                  "Insert ksi stats here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                width = 4,
+                title = "Vehicle Class vs Casualty Role Graph",
+                "Insert vehicle class vs casualty role graph here"
+              ),
+              box(
+                width = 4,
+                title = "Junction and Road Stats",
+                "Insert junction and role stats here"
+              ),
+              box(
+                width = 4,
+                title = "Collision Year Line Graph",
+                "Insert collision year line graph here"
+              )
+            ),
+
+            fluidRow(
+              box(width = 6,
+                  title = "Contributory Factors Stats",
+                  "Insert contributory factors stats here"
+              ),
+              box(width = 6,
+                  title = "Accidents by Road Length Stats",
+                  "Insert accidents by road length stats here"
+              )
+            )
+          ),
+
+          # Vehicle w/ Peds tab
+          tabPanel(
+            value = "vehicle_with_pedestrians",
+            title = "Vehicle w/ Peds",
+
+            fluidRow(
+              box(
+                width = 4,
+                title = "Area Filter",
+                "Insert area filter here"
+              ),
+              box(
+                width = 4,
+                title = "Year Filter",
+                "Insert year filter here"
+              ),
+              box(
+                width = 4,
+                title = "All/ KSI Filter",
+                "Insert all/ksi filter here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                width = 6,
+                title = "Collision Map",
+                "Insert collison map here"
+              ),
+              box(width = 6,
+                  title = "KSI Stats",
+                  "Insert ksi stats here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                width = 3,
+                title = "Vehicle Class Stats",
+                "Insert vehicle class stats here"
+              ),
+              box(
+                width = 3,
+                title = "Vehicle Movement Stats",
+                "Insert vehicle movement stats here"
+              ),
+              box(
+                width = 3,
+                title = "Pedestrian Action Stats",
+                "Insert padestrian action stats here"
+              ),
+              box(
+                width = 3,
+                title = "Junction and Road Stats",
+                "Insert junction and road stats here"
+              )
+            ),
+
+            fluidRow(
+              box(width = 12,
+                  title = "Contributory Factors Stats",
+                  "Insert contributory factors stats here"
+              )
+            )
+          ),
+
+          # Vehicle w/ Cycles tab
+          tabPanel(
+            value = "vehicle_with_bicycles",
+            title = "Vehicle w/ Cycles",
+
+            fluidRow(
+              box(
+                width = 4,
+                title = "Area Filter",
+                "Insert area filter here"
+              ),
+              box(
+                width = 4,
+                title = "Year Filter",
+                "Insert year filter here"
+              ),
+              box(
+                width = 4,
+                title = "All/ KSI Filter",
+                "Insert all/ksi filter here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                width = 6,
+                title = "Collision Map",
+                "Insert collison map here"
+              ),
+              box(width = 6,
+                  title = "KSI Stats",
+                  "Insert ksi stats here"
+              )
+            ),
+
+            fluidRow(
+              box(
+                width = 3,
+                title = "Vehicle Class Stats",
+                "Insert vehicle class stats here"
+              ),
+              box(
+                width = 3,
+                title = "Vehicle Movement Stats",
+                "Insert vehicle movement stats here"
+              ),
+              box(
+                width = 3,
+                title = "Cyclist Action Stats",
+                "Insert cyclist action stats here"
+              ),
+              box(
+                width = 3,
+                title = "Road Stats",
+                "Insert road stats here"
+              )
+            ),
+
+            fluidRow(
+              box(width = 12,
+                  title = "Contributory Factors Stats",
+                  "Insert contributory factors stats here"
+              )
+            )
+          )
+
+        ) # tabBox
       ) # tabItem
     ) # tabItems
   ) # dashboardBody
