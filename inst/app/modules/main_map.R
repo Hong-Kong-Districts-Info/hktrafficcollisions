5d03cf6ccff166d293d2b5735bc3c488b2823637
Kenneth Wong <khwongk12@gmail.com>
2021-05-10 20:06:07 +0800

    feat: Use CartoDB custom tile

--- inst/app/modules/main_map.R
+++ inst/app/modules/main_map.R
@@ -1,7 +1,9 @@
 
+CARTODB_POSITRON_TILE_URL <- "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+
 output$main_map <- renderLeaflet({
   leaflet() %>%
-    addTiles() %>%
+    addTiles(urlTemplate = CARTODB_POSITRON_TILE_URL) %>%
     setView(114.2, 22.3, zoom = 12)
 })
 
