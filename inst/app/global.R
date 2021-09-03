68bf7f10c3a353ce79f5c77d9b1a5c1f9c467d85
Kenneth Wong <khwongk12@gmail.com>
2021-09-04 07:42:40 +0800

    feat: Add by-data-cateogry color scheme for charts

--- inst/app/global.R
+++ inst/app/global.R
@@ -44,3 +44,4 @@ tmap_options(basemaps = c("CartoDB.Positron", "Stamen.TonerLite", "OpenStreetMap
 # Color scheme ------------------------------------------------------------
 
 SEVERITY_COLOR = c(Fatal = "#230B4C", Serious = "#C03A51", Slight = "#F1701E")
+CATEGORY_COLOR = setNames(as.list(c("#67B7DC", "#A367DC", "#FFAE12")), c("accidents", "casualties", "vehicles"))
