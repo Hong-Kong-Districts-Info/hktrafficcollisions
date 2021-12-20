cbf77349800e4684d5225db567ffbf7f39b5fec0
Kenneth Wong <khwongk12@gmail.com>
2021-12-21 04:42:14 +0800

    feat: Change palette of collision gridded count map to purple

--- inst/app/modules/district_dsb_all.R
+++ inst/app/modules/district_dsb_all.R
@@ -128,7 +128,7 @@ output$ddsb_all_collision_heatmap = renderTmap({
   tm_shape(all_grid_count()) +
     tm_fill(
       col = "n_colli",
-      palette = "YlOrRd",
+      palette = "Purples",
       n = 10,
       style = "cont",
       title = "Number of collisions",
@@ -138,7 +138,7 @@ output$ddsb_all_collision_heatmap = renderTmap({
       # disable popups
       popup.vars = FALSE,
     ) +
-    tm_borders(col = "white", lwd = 0.7)
+    tm_borders(col = "#232323", lwd = 0.7)
 
 })
 
