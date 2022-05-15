166ff56b4473ea1fa406a3584ccd6487fa1b6599
Kenneth Wong <khwongk12@gmail.com>
2022-05-16 01:25:00 +0800

    refactor: Move steps involving data manipulation to separate file

--- inst/app/modules/hotzones.R
+++ inst/app/modules/hotzones.R
@@ -55,22 +55,6 @@ observe({
   })
 })
 
-
-hotzone_out_df = hotzone_streets %>%
-  st_centroid() %>%
-  st_transform(crs = st_crs(4326)) %>%
-  mutate(lng = sf::st_coordinates(.)[,1], lat = sf::st_coordinates(.)[,2]) %>%
-  # create the required data zooming into the feature with gomap.js
-  mutate(zoom_in_map_link =
-           paste('<a class="go-map" href=""',
-                 'data-lat="', lat, '" data-lng="', lng,
-                 '"><i class="fas fa-search-plus"></i></a>',
-                 sep="")
-         ) %>%
-  st_set_geometry(NULL) %>%
-  dplyr::select(-c(lat, lng)) %>%
-  dplyr::relocate(Area_RK, zoom_in_map_link)
-
 output$hotspots_table = renderDataTable({
 
   # `rownames` needs to be consistent with `DT::datatable` option
--- inst/app/modules/main_map.R
+++ inst/app/modules/main_map.R
@@ -1,37 +1,3 @@
-# Get information of all types of vehicles involved in the accidents to show in popup
-# TODO: prepare following mutated dataset before running shiny to save loading time?
-hk_vehicles_involved <- hk_vehicles %>%
-  group_by(Serial_No_) %>%
-  summarize(vehicle_class_involved = paste(sort(unique(Vehicle_Class)), collapse = ", "))
-
-# Get casualty role involved in each accident to show in popup
-casualty_role_n = hk_casualties %>% count(Serial_No_, Role_of_Casualty)
-
-accidents_cas_type <- casualty_role_n %>%
-  pivot_wider(
-    id_cols = Serial_No_,
-    names_from = Role_of_Casualty,
-    values_from = n, values_fill = 0
-  ) %>%
-  rename(cas_ped_n = Pedestrian, cas_pax_n = Passenger, cas_dvr_n = Driver)
-
-
-# Add date floored to first day of the month for easier month filter handling
-hk_accidents <- mutate(hk_accidents, year_month = floor_date_to_month(Date_Time))
-
-hk_accidents_join <- hk_accidents %>%
-  left_join(accidents_cas_type, by = "Serial_No_") %>%
-  left_join(hk_vehicles_involved, by = "Serial_No_") %>%
-  # Show full name of district in popup of maps
-  left_join(data.frame(DC_Abbr = DISTRICT_ABBR, DC_full_name = DISTRICT_FULL_NAME),
-            by = c("District_Council_District" = "DC_Abbr"))
-
-hk_accidents_valid <- filter(hk_accidents_join, !is.na(latitude) & !is.na(longitude))
-
-# Leaflet default expect WGS84 (crs 4326), need custom CRS for HK1980 Grid (crs 2326)
-# https://rstudio.github.io/leaflet/projections.html
-hk_accidents_valid_sf <- st_as_sf(x = hk_accidents_valid, coords = c("longitude", "latitude"), crs = 4326, remove = FALSE)
-
 output$main_map <- renderLeaflet({
   overview_map <- leaflet(options = leafletOptions(minZoom = 11, preferCanvas = TRUE)) %>%
     setView(lng = 114.2, lat = 22.3, zoom = 12) %>%
