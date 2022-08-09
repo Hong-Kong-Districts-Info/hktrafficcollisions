6fe686553e611060c2fe714367dc952b367e2982
Kenneth Wong <khwongk12@gmail.com>
2022-08-10 03:33:41 +0800

    feat: Refine appearance of the glossary table

--- inst/app/modules/project_info.R
+++ inst/app/modules/project_info.R
@@ -1,6 +1,14 @@
 # Terminology table
 output$terminology_table = renderDataTable({
   datatable(
-    terminology
+    terminology,
+    rownames = FALSE,
+    # Show all rows in one single page
+    options = list(
+      # only show the filtering box (f) and the table (t)
+      dom = 'ft',
+      # Show all rows in one single page
+      pageLength = 100
+      )
     )
 })
