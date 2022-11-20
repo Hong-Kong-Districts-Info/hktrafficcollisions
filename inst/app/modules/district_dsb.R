856a349d38d6d016977f3351d45b33436a266349
Kenneth Wong <khwongk12@gmail.com>
2022-11-20 15:40:55 +0800

    refactor: Move `ddsb_filter_hk_accidents` to top file

--- inst/app/modules/district_dsb.R
+++ inst/app/modules/district_dsb.R
@@ -10,3 +10,30 @@ output$dsb_filter_ui = renderUI({
     selected = "CW"
   )
 })
+
+
+# Return filtered hk_accidents dataframe according to users' selected inputs
+ddsb_filtered_hk_accidents = reactive({
+  # filter by users' selected district
+  # FIXME: Temp workaround to fix non-initialised value when district filter renders in server side
+  ddsb_district_filter = if (is.null(input$ddsb_district_filter)) "CW" else input$ddsb_district_filter
+  hk_accidents_filtered = filter(hk_accidents, District_Council_District == ddsb_district_filter)
+
+  # filter by users' selected time range
+  hk_accidents_filtered = filter(hk_accidents_filtered, Year >= input$ddsb_year_filter[1] & Year <= input$ddsb_year_filter[2])
+
+  # remove slightly injured collisions if user select "KSI only" option
+  if (input$ddsb_ksi_filter == "Killed or Seriously Injuries only") {
+    hk_accidents_filtered = filter(hk_accidents_filtered, Severity != "Slight")
+  }
+
+  # Show only collisions with valid lng/lat
+  hk_accidents_filtered = hk_accidents_filtered %>%
+    filter(!is.na(Grid_E) & !is.na(Grid_N)) %>%
+    st_as_sf(coords = c("Grid_E", "Grid_N"), crs = 2326, remove = FALSE)
+
+  print(nrow(hk_accidents_filtered))
+
+  hk_accidents_filtered
+
+})
