7f07a8c51313a2b85018ac58f27b281eebf94c4e
Kenneth Wong <khwongk12@gmail.com>
2022-11-20 15:37:35 +0800

    feat: Add overarching module file of dashboard

---
+++ inst/app/modules/district_dsb.R
@@ -0,0 +1,1 @@
+# Fundamental reactive functions and UI for the district dashboard
--- inst/app/server.R
+++ inst/app/server.R
@@ -34,6 +34,7 @@ server <- function(input, output, session) {
 
 
   # ----- TAB: Dashboard ----- #
+  source(file = "modules/district_dsb.R", local = TRUE)
   source(file = "modules/district_dsb_all.R", local = TRUE)
   source(file = "modules/district_dsb_ped.R", local = TRUE)
   source(file = "modules/district_dsb_cyc.R", local = TRUE)
