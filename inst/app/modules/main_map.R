d521d4066e0603cf3ad22b047623eb588d71ee2a
Kenneth Wong <khwongk12@gmail.com>
2025-02-02 03:49:52 +0800

    feat: Show exceed warning message as modal window instead of notification in bottom right

--- inst/app/modules/main_map.R
+++ inst/app/modules/main_map.R
@@ -185,29 +185,22 @@ filter_collision_data <-
 
       data_filtered <- filter(data_filtered, serial_no %in% accient_w_selected_veh_vct)
 
-      # Show at most 20,000 points on the map to ensure performance
-      if (nrow(data_filtered) > 20000) {
+      # Show at most 10,000 points on the map to ensure performance
+      if (nrow(data_filtered) > 10000) {
 
-        showNotification(
-          paste(
-            "
-            地圖不能顯示所有符合篩選條件的車禍。
-            此地圖只可以同時顯示最多 20,000 宗車禍，而現有篩選條件包含超過 20,000 宗車禍。
-            地圖只會顯示首 20,000 宗符合條件的車禍。
-            請更改篩選條件（如刪除地區，縮短日期範圍）以顯示所有符合篩選條件之車禍。
-            ",
-            "
-            The map cannot show all collisions matching the requirements.
-            The total number of collisions included in current filter settings exceeds the rendering capacity (20,000 points) of the map.
-            Only the first 20,000 records are shown on the map.
-            Change the filters (e.g. remove districts outside your area of interest, shortern the time frame) to show all filtered records.
-            "
-            , collapse = "<br/>"),
-          type = "error",
-          duration = NULL,
-        )
+        showModal(modalDialog(
+          title = "⚠️ 地圖無法顯示所有符合篩選條件的車禍 | The map cannot display all collisions  matching your filter criteria",
+          tags$ul(
+            tags$li("此地圖只可以同時顯示最多 10,000 宗車禍，而符合您目前篩選條件的車禍已超過此數量。"),
+            tags$li("請調整篩選條件（如縮小地區範圍／縮短日期範圍）以顯示所有符合篩選條件之車禍。"),
+            br(),
+            tags$li("The map can only display up to 10,000 collisions at once, and your current filters exceed this limit."),
+            tags$li("Adjust your filters (e.g., narrow the districts/shorten the date range) to view all qualifying collisions.")
+          ),
+          easyClose = TRUE
+        ))
 
-        return(data_filtered[1:20000,])
+        return(data_filtered[1:10000,])
       }
 
       data_filtered
